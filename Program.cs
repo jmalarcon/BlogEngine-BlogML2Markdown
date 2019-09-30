9deebbdbb0874f3a45252b6434ea4daa3fd15dfb
Jose Alarcón <jmalarcon@jasoft.org>
2019-09-30 18:16:09 +0200

    Initial enhanced version

---
+++ Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace BlogMLToMarkdown
+{
+    class Program
+    {
+        //CHANGE this to adapt it to your own needs
+        private const string postFormat = @"---
+layout: post.html
+title: {0}
+published: {1}
+date: {2}
+excerpt: {3}
+categories: {4}
+tags: {5}
+authors: {6}
+image: {7}
+---
+
+{8}
+";
+        private const string blogMLNamespace = "http://www.blogml.com/2006/09/BlogML";
+
+        static void Main(string[] args)
+        {
+            string documentContent = null;
+            using(var sr = new StreamReader(args[0]))
+            {
+                documentContent = sr.ReadToEnd();
+            }
+
+            var document = XDocument.Load(XmlReader.Create(new StringReader(documentContent), new XmlReaderSettings
+                {
+                    IgnoreComments = true,
+                    CheckCharacters = false,
+                }));
+
+            var allCategories = document.Root.Elements(XName.Get("categories", blogMLNamespace))
+                .Elements()
+                .ToArray();
+
+            var posts = document.Root.Elements(XName.Get("posts", blogMLNamespace))
+                .Elements()
+                .ToArray();
+
+            int numPosts = posts.Length;
+            int numProcessed = 0;
+
+            foreach (var post in posts)
+            {
+                var dateCreated = post.Attribute("date-created").Value;
+                var title = WrapInDoubleQuotes(post.Descendants(XName.Get("title", blogMLNamespace)).First().Value);
+
+                numProcessed++;
+                Console.WriteLine("Post {0} of {1}: {2}", numProcessed, numPosts, title);
+
+                string folder = "posts";    //By default, save posts in this folder
+                string postType = GetFileNameNoExt(post.Attribute("type").Value.ToLower());
+                if (postType != "normal")
+                    folder = "other";   //Folder to save everything that is not a post (articles, for example)
+
+                string filename = GetFileNameNoExt(post.Attribute("post-url").Value);
+
+                bool isPublished = post.Attribute("is-published").Value.ToLower() == "true";
+                bool isApproved = post.Attribute("approved").Value.ToLower() == "true";
+                if (!isPublished || !isApproved)
+                    folder = "NotPublished";
+
+                //Check if it has an excerpt
+                bool hasExcerpt = post.Attribute("hasexcerpt").Value.ToLower() == "true";
+                string excerpt = "";
+                if (hasExcerpt)
+                    try
+                    {
+                        //Not always has an excerpt although it says so
+                        excerpt = WrapInDoubleQuotes(post.Descendants(XName.Get("excerpt", blogMLNamespace)).First().Value);
+                    }
+                    catch
+                    {
+                    }
+
+                var content = post.Descendants(XName.Get("content", blogMLNamespace)).First().Value;
+                var url = post.Attribute("post-url").Value;
+                var postname = url.Substring(url.LastIndexOf("/") + 1).Replace(".aspx", "");
+               
+                var rawCategories = post.Descendants(XName.Get("category", blogMLNamespace))
+                    .Select(c1 => c1.Attribute("ref").Value)
+                    .ToArray();
+
+                var categories = ConvertoToYamlArray(allCategories.Where(c1 => rawCategories.Any(c2 => c2 == c1.Attribute(XName.Get("id")).Value))
+                    .Select(c1 => WrapInDoubleQuotes(c1.Elements().First().Value))
+                    .ToArray());
+
+                var tags = ConvertoToYamlArray(post.Descendants(XName.Get("tag", blogMLNamespace))
+                    .Select(c1 => WrapInDoubleQuotes(c1.Attribute("ref").Value))
+                    .ToArray());
+
+                var authors = ConvertoToYamlArray(post.Descendants(XName.Get("author", blogMLNamespace))
+                    .Select(c1 => WrapInDoubleQuotes(c1.Attribute("ref").Value))
+                    .ToArray());
+
+                var markdown = FormatCode(ConvertHtmlToMarkdown(content));
+                //BlogEngine specific: remove the image.axd handler for images and substitute it for the "images folder
+                //Change the folder name if you need to
+                markdown = markdown.Replace("/image.axd?picture=/", "images/");
+                markdown = markdown.Replace("/image.axd?picture=", "images/");  //Not all start with a slash
+
+                //Extract first image and use it as the highlighted image of the post
+                string image = "";
+                Regex reMDImages = new Regex(@"\!\[.*?\]\(([^\)]*?)("".*?""){0,1}\)", RegexOptions.Singleline);
+                Match firstImgMatch = reMDImages.Match(markdown);
+                if (firstImgMatch.Success)
+                {
+                    image = firstImgMatch.Groups[1].Value;
+                }
+
+                var blog = string.Format(postFormat, 
+                    title, //{0}
+                    isPublished && isApproved ? "true" : "false" , //{1}
+                    dateCreated, //{2}
+                    excerpt, //{3}
+                    categories, //{4}
+                    tags, //{5}
+                    authors,//{6}
+                    image, //{7}
+                    markdown);  //{8}
+
+                if (!Directory.Exists("output\\" + folder))
+                {
+                    Directory.CreateDirectory("output\\" + folder);
+                }
+                using (var sw = File.CreateText ("output\\" + folder + "\\" + filename + ".md"))
+                {
+                    sw.Write(blog);
+                };
+                
+            }
+            Console.WriteLine("All posts processed and generated!!");
+            Console.ReadLine();
+        }
+
+        private static object ConvertoToYamlArray(string[] arr)
+        {
+            return string.Format("[{0}]", string.Join(", ", arr));
+        }
+
+        private static string WrapInDoubleQuotes(string value)
+        {
+            return @"""" + value + @"""";
+        }
+
+        private static string GetFileNameNoExt(string path)
+        {
+            int slashPos = path.LastIndexOf('/');  //Position of folder separator
+            if (slashPos >= 0)
+                path = path.Substring(slashPos + 1);
+            int dotPos = path.LastIndexOf('.');
+            //If there's no final point
+            if (dotPos == -1)
+                return path;    //The path is a file without extension
+            //Temove everyting after the point position (included)
+            return path.Substring(0, dotPos);
+        }
+
+        static string ConvertHtmlToMarkdown(string source)
+        {
+            string args = String.Format(@"-r html -t markdown");
+
+            var startInfo = new ProcessStartInfo("pandoc.exe", args)
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false
+            };
+
+            var process = new Process { StartInfo = startInfo };
+            process.Start();
+
+            var inputBuffer = Encoding.UTF8.GetBytes(source);
+            process.StandardInput.BaseStream.Write(inputBuffer, 0, inputBuffer.Length);
+            process.StandardInput.Close();
+
+            process.WaitForExit(2000);
+            using (var sr = new StreamReader(process.StandardOutput.BaseStream))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        static readonly Regex _codeRegex = new Regex(@"~~~~ \{\.csharpcode\}(?<code>.*?)~~~~", RegexOptions.Compiled | RegexOptions.Singleline);
+
+        static string FormatCode(string content)
+        {
+            return _codeRegex.Replace(content, match =>
+            {
+                var code = match.Groups["code"].Value;
+                return "```" + GetLanguage(code) + code + "```";
+            });
+        }
+
+        static string GetLanguage(string code)
+        {
+            var trimmedCode = code.Trim();
+            if (trimmedCode.Contains("<%= ") || trimmedCode.Contains("<%: ")) return "aspx-cs";
+            if (trimmedCode.StartsWith("<script") || trimmedCode.StartsWith("<table")) return "html";
+            return "csharp";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # BlogEngine-BlogML2Markdown
-C# command-line tool that converts a BlogML.xml file from BlogEngine to markdown files for MIIS, Jekyll, Hugom, etc...
+
+C# command-line tool that converts a BlogML `.xml` file from BlogEngine to markdown files for [MIIS](https://github.com/jmalarcon/miis), Jekyll, Hugo, etc...
+
+It uses [pandoc](https://pandoc.org) for transforming HTML into Markdown. It's not 100% perfect but you will get pretty good markdown files with very few issues. **This tool does not export comments** (yet).
+
+>This was originally **based on this [repo by Pablo Cibraro](https://github.com/pcibraro/BlogMLToMarkdown)**, but couldn't make it work for pull requests, and it seems like abandoned. 
+
+I've improved a lot of details in the export process and data and made it more BlogEngine specific although it'll work with any BlogML generating blog system.
+
+## How to use it
+
+>**Important**: I'm attaching a compiled version in the "release" section of this repo. However I highly recommend to run it from Visual Studio, using the source code. If something fails you should be able to see exactly what happened and fix it. This is a rough and quick exporter, so maybe some issues may arise. On the other hand you'll be able to tweek the results, for example, changing the name of the `images` folder, or th enames of the resulting markdown files folders.
+
+### 1. Export your blog to BlogML
+
+Go to the administration area of your BlogEngine blog and access the `Configuration·Advanced`section. Scroll down to the latest option `Export`. Clicking on this button will generate an `.xml` file on the fly that you must download to your hard drive.
+
+### 2. Run `BlogMLToMarkdown.exe`
+
+You need to specify the name or path of the .xml file as a parameter. If the file is in the same folder as the `.exe`, just the name is enough. If not, you should specify a full or relative path:
+
+```bash
+BlogMLToMarkdown.exe C:\Temp\MyBlogExport.xml
+```
+
+If you run the program from within Visual Studio, you should change the debug parameters for the project, by clicking with the secondary mouse button over the project root node:
+
+![Project properties in context menu](imgs/vs-props-01.png)
+
+and then going to `Debug` and `Start options`:
+
+![Project properties in context menu](imgs/vs-props-02.png)
+
+where you can put the path to your file.
+
+### 3. Run the app and wait for a while
+
+It taks around 10 seconds per post depending on it's length due to the use of pandoc, but since this is one in a lifetime, it shouldn't matter a lot.
+
+You'll see every post title and a progress while they are being processed and, at the end, you'll get an `output` folder with a maximum of three subfolders under it:
+
+- `posts`: in this folder you'll find all the files that are marked as posts in BlogEngine and are published. So, the posts for your new blog!
+- `other`: other kinds of publications in your current site, mainly content marked as "Page" in BlogEngine.
+- `NotPublished`: Everything that is not published or approved is generated into this folder. It makes easy to find all your unpublished content and decide if you want to keep it in your new site or not (or just make a backup of it in Markdown format).
+
+### 4. Copy images
+
+By default, the app assumes your images are located into an `images` folder under your `posts` folder. Therefore the tool changes all the `image.axd` kind of image pointers to the `images/` folder.
+
+So, to crrectly have all your images in your new site, just copy the contents of the `App_Data/files` folder in your previous BlogEngine server, into an `images` folder inside the `posts` folder. That's it!
+
+The tool adds an `image` field into the resulting `.md` files front-matter pointing to the first image inside your content, so that it will be used as the main distinguished image of each post, because a lot of MIIS/Jekyll templates use this field with that purpose.
+
+>**Note**: The provided `test.xml` file is in Spanish, from my old blog, sorry. It only has 3 sample posts, but I've tested it with almost one thousand posts in two different BlogEngine blogs and it went OK.
+
+Hope this helps!
